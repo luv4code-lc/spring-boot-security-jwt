4bd057df9942294d6ed7e697be3048b15219f061
luv4code-lc <madhav.p.akv@gmail.com>
2022-02-16 14:16:57 +0530

    creating spring boot application skeleton

---
+++ src/test/java/com/luv4code/spring/security/api/SpringBootSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv4code.spring.security.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
